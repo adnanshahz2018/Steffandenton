c7a0290e6c19eb702ec05356e242076fe710eebd
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2021-03-23 19:07:38 +0500

    Implemented DigiKey Login Automation

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 
 #  Python imports
-from time import sleep
 import xlsxwriter 
 import xlsxwriter 
 import pandas as pd
 import openpyxl as op
 from numpy import nan
 import openpyxl as op
+from time import sleep
 from bs4 import BeautifulSoup
+
+# selenium imports
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from random_user_agent.user_agent import UserAgent
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from random_user_agent.params import SoftwareName, OperatingSystem
+
 
 class octopart:
-    filename = 'results.xlsx'
+    filename = 'output.xlsx'
     browser = None
 
     def __init__(self):
         self.create_excel_file()
+        self.digikey_login()
 
     def scrape(self, partnumber):
-        self.browser = webdriver.Chrome('chromedriver.exe') 
-        self.digikey_login()
+        # print('\n SCRAPING  . . . \n')
+        # self.browser = webdriver.Chrome('chromedriver.exe') 
         url = 'https://octopart.com/search?q=' + partnumber + '&currency=USD&specs=0'
         self.browser.get(url)
         bs = self.browser.find_elements_by_tag_name("button")  
@@ -37,7 +46,6 @@ class octopart:
                 moq = tr.find('td', attrs={'class': 'jsx-3318793868'})
                 price = tr.find('td', attrs={'class': 'jsx-1795508439'})
                 sku = tr.find('td', attrs={'class': 'jsx-2389699081 sku'})
-                
                 moq = moq.get_text()
                 sku = sku.find('a').get_text()
                 stock = str(stock.get_text()).replace(',','')
@@ -45,19 +53,24 @@ class octopart:
                 except: price = 'None'
                 desc = 'None'
                 try:
-                    a = tr.find('a', attrs={'class': 'jsx-2100737765 click-url'})
-                    link = a['href']
-                    # print(link)
+                    link = tr.find('a', attrs={'class': 'jsx-2100737765 click-url'})['href']
                     self.browser.get(link)
                     desc = self.digikey(self.browser.page_source)
-                except: pass
+                except: 
+                    sleep(15)
+                    try:
+                        link = tr.find('a', attrs={'class': 'jsx-2100737765 click-url'})['href']
+                        self.browser.get(link)
+                        desc = self.digikey(self.browser.page_source)
+                    except: pass
                 
                 print('Digi-Key:')
                 print('\tpartID = ', partnumber)
-                print('\tSupplier Code = ', sku)
-                print('\tMOQ = ', moq)
-                print('\tStock = ', int(stock))
-                print('\tPrice = ', price)
+                print('\tS Code = ', sku)
+                print('\tMOQ    = ', moq)
+                print('\tStock  = ', int(stock))
+                print('\tPrice  = ', price)
+                print('\tDesc   = ', desc)
                 self.write_to_excel(partnumber, desc, 'Digi-Key', link, sku, stock, moq, price)
                 
             # if str(tr.get_text()).__contains__('Mouser') :
@@ -84,23 +97,44 @@ class octopart:
                 # print('\tPrice = ', price)
                 # self.write_to_excel(partnumber, 'Farnell', value, price)
     
-        sleep(5)
-        print('\n\n')
+    def close_browser(self):
         self.browser.quit()
 
-    def digikey_login(self):   
-        self.browser = webdriver.Chrome('chromedriver.exe') 
+    def digikey_login(self):
         login_url = 'https://auth.digikey.com/as/authorization.oauth2?response_type=code&client_id=pa_wam&redirect_uri=https%3A%2F%2Fwww.digikey.com%2Fpa%2Foidc%2Fcb&state=eyJ6aXAiOiJERUYiLCJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2Iiwia2lkIjoiMnMiLCJzdWZmaXgiOiJqMlF6T0guMTYxNjE1NjE1MCJ9..2GHWxHGWNbzHWGmhnZy-bg.mOYu2qAf3Dpw5XtEa9k-T9dAkymsZddvUugkoGWVZC_AtbxvCJcGU1p6T6nW9tDQPZ5EhrOs5T5EPtDLhcRepDN0G9_bVYIvlsQYzBsk9gc.GA5-rxU38vBdh4ZUJwVIww&nonce=OzIsYKw5g25-48HBJ9SdSDok-fYrnuGMm1-_hR03u7w&scope=openid%20address%20email%20phone%20profile&vnd_pi_requested_resource=https%3A%2F%2Fwww.digikey.com%2FMyDigiKey&vnd_pi_application_name=DigikeyProd-Mydigikey'
+        try:
+            software_names = [SoftwareName.CHROME.value]
+            operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
+            user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+            user_agent = user_agent_rotator.get_random_user_agent()
 
-        self.browser.get(login_url)
-        elementID = self.browser.find_element_by_id('username')
-        elementID.send_keys("adnanshahz2015@gmail.com")
-        elementID = self.browser.find_element_by_id('password')
-        elementID.send_keys("zeshan2015")
-        elementID.submit()
+            chrome_options = Options()           
+            chrome_options.add_argument(f'user-agent={user_agent}')
+            # chrome_options.add_argument('--disable-extensions')
+            # chrome_options.add_argument('--profile-directory=Default')
+            # chrome_options.add_argument("--disable-plugins-discovery")
+            # chrome_options.add_argument("--start-maximized")
+            # chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            # chrome_options.add_experimental_option('useAutomationExtension', False)
+            # chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-        self.browser.get('https://www.digikey.com/en/products/detail/ERJ-8ENF1001V/P1.00KFCT-ND/89591?utm_campaign=buynow&utm_medium=aggregator&curr=usd&utm_source=octopart')
-        self.browser.quit()
+            driver = webdriver.Chrome(options=chrome_options)
+            driver.delete_all_cookies()
+            driver.set_window_position(0,0)
+            driver.maximize_window()
+            self.browser = driver
+            self.browser.get(login_url)
+            elementID = self.browser.find_element_by_id('username')
+            elementID.send_keys("adnanshahz2015@gmail.com")
+            elementID = self.browser.find_element_by_id('password')
+            elementID.send_keys("zeshan2015")
+            elementID.submit()
+
+            self.browser.get('https://digikey.com/')
+            self.browser.find_element_by_id('my_digikey_logged_in').click()
+            sleep(7)
+        except: 
+            print("\nCan't Login to DigiKey - - -\n")
 
     def farnell_login(self):   
         login_url = 'https://uk.farnell.com/webapp/wcs/stores/servlet/LogonForm?myAcctMain=1&catalogId=15001&storeId=10151&langId=44'
@@ -116,10 +150,6 @@ class octopart:
         sleep(5)
 
     def digikey(self, source=None):
-        browser = webdriver.Chrome('chromedriver.exe') 
-        browser.get('https://octopart.com/click/track?country=PK&ct=offers&ppid=55423196&sid=459&sig=0c18431&vpid=188064011')
-        source = browser.page_source
-
         soup = BeautifulSoup(source, features='lxml')
         tbody = soup.find('tbody', attrs={'class' : 'MuiTableBody-root'})
         tr = tbody.find_all('tr')[4]
@@ -149,9 +179,9 @@ class octopart:
         wb.close()
 
 
-def selected_countries():
+def selected_part_numbers():
     part_list = []
-    file_data =  pd.read_excel('data.xlsx', 'data')
+    file_data =  pd.read_excel('input.xlsx', 'data')
     datas = file_data['manufacture part number']
     for i in datas.index:
         part_list.append(datas[i])
@@ -159,20 +189,10 @@ def selected_countries():
 
 if __name__ == '__main__':
     obj = octopart()
-    obj.digikey_login()
-    # part_list = selected_countries() 
-    # print(part_list)   
-    # for part in part_list:
-    #     obj.scrape(part)
-    #     sleep(5)
-    #     break
+    part_list = selected_part_numbers() 
+    print(part_list)   
+    for part in part_list:
+        obj.scrape(part)
+        sleep(7)
+    obj.close_browser()
 
-
-"""
-
-We need the program to see the batch quantity needed and 
-whether farnell, digikey, arrow or mouser have the stock available needed for the batch. 
-if YES then it needs to pick the lowest price. 
-if NO stock it would need to tell us that there was no stock available
-
-"""
